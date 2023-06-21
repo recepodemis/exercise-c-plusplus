9f7bda6b8ae31d6058048a669159836f5ea3cee1
recep ödemiş <53352764+recepodemis@users.noreply.github.com>
2023-06-21 09:10:19 +0300

    Update classExercise.cpp

--- classExercise.cpp
+++ classExercise.cpp
@@ -1,13 +1,3 @@
-/*Goal: practice creating and using a class
-** Use the class 'Dog' to create to 
-** instances of the class, dog1 and dog2.
-** Assign dog1 the name: Trixie
-** Assign dog2 the name: Kali
-** Assign dog1 the license #1234
-** Assign dog2 the license #5678
-** Print the information for each dog.
-*/
-
 
 #include"classExercise.hpp"
 
