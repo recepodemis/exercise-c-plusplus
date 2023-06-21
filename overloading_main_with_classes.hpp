84f68a742cadeede785ff97b3108780c508097b2
recep ödemiş <53352764+recepodemis@users.noreply.github.com>
2023-06-21 09:10:51 +0300

    Update overloading_main_with_classes.hpp

--- overloading_main_with_classes.hpp
+++ overloading_main_with_classes.hpp
@@ -1,12 +1,3 @@
-/*Create a class that has four constructors:
-* one that accepts no input parameters
-* one that accepts name
-* one that accepts license number
-* one that accepts name and license number
-* The default values are:
-name = NA
-license = 0
-*/
 
 #include<iostream>
 #include<string>
