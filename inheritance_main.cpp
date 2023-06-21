de9b9485245bedb0b6720097915f7f9bbbbe1321
recep ödemiş <recepodemis6@gmail.com>
2023-06-21 09:48:16 +0300

    with inheritance exercise

---
+++ inheritance_main.cpp
@@ -0,0 +1,12 @@
+
+#include "inheritance_main.hpp"
+
+int main()
+{
+    Rose r1;
+    r1.setBloomTime("spring");
+    r1.setFragrance("strong");
+    
+    cout<<"r1 blooms in "<<r1.getBloomTime()<<", "<<r1.getFragrance();
+    return 0;
+}
